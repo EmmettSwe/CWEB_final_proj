29211298fecef5e53edb21d00f6f7bba6b15f35d
Swejda9940 <Swejda9940@saskpolytech.ca>
2024-11-17 19:54:06 -0600

    CORS is now fuctional and so is recipe CRUD functionality

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import * as express from "express"
 import * as bodyParser from "body-parser"
 import { Request, Response } from "express"
 import { AppDataSource } from "./data-source"
+import * as createError from 'http-errors';
 import { Routes } from "./routes"
-import {Recipe} from "./entity/Recipe";
-import {RecipeController} from "./controller/RecipeController";
+import * as cors from 'cors';
+// cors options
+const corsOptions ={
+    origin: /localhost\:\d{4}$/i, // localhost any 4 digit port
+    credentials: true, // needed to set and return cookies
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+    methods: 'GET,PUT,POST,DELETE',
+    maxAge: 43200, // 12 hours
+};
 
 AppDataSource.initialize().then(async () => {
 
@@ -12,7 +20,13 @@ AppDataSource.initialize().then(async () => {
     const app = express()
     app.use(bodyParser.json())
 
-    // register express routes from defined application routes
+    app.use(cors(corsOptions)); // enable CORS for all handlers
+    app.use((req: express.Request, res: express.Response, next: express.NextFunction ) => {
+        if (req.xhr && req.accepts('application/json')) next();
+        else next(createError(406));
+    });
+    app.options('*', cors(corsOptions));
+
     Routes.forEach(route => {
         (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
             const result = (new (route.controller as any))[route.action](req, res, next)
@@ -24,10 +38,15 @@ AppDataSource.initialize().then(async () => {
             }
         })
     })
-
-    // setup express app here
-    // ...
-
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        next(createError(404));
+    });
+// error handler
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({status: err.status, message: err.message, stack: err.stack.split(/\s{4,}/)});
+    });
     // start express server
     app.listen(3000)
 
